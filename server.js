48339683779f4c4bb0d912ff0e5192bd4b71baac
Dianaiminza <dianaiminza.99@gmail.com>
2021-09-01 22:21:13 +0300

    my first commit

--- server.js
+++ server.js
@@ -29,11 +29,23 @@ app.use(bodyParser.json());
 app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
-app.use(express.static(path.join(__dirname, '//frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}//frontend/build/index.html`));
-});
+// app.use(express.static(path.join(__dirname, '//frontend/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}//frontend/build/index.html`));
+// });
 
-app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
-});
+// app.listen(config.PORT, () => {
+//   console.log('Server started at http://localhost:5000');
+if(process.env.NODE_ENV ==='production'){
+  app.use(express.static('frontend/build'));
+  
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'));
+  
+  });
+  }
+  
+  app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+  
