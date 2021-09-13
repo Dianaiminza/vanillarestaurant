84e89aa772d916b79f9274197d28d394345d49a5
Dianaiminza <dianaiminza.99@gmail.com>
2021-09-13 13:15:17 +0300

    my first commit

--- frontend/src/screens/FooterScreen.js
+++ frontend/src/screens/FooterScreen.js
@@ -4,31 +4,8 @@ export default class FooterScreen extends Component {
     render() {
         return (
             <div className="footer">
-                {/* <footer className="footer">All right reserved.</footer> */}
-                <div className="col-lg-3">
-<div className="widget">
-<h3>Contact</h3>
-<address>Vanilla Restaurant Langata 589, Nairobi</address>
-<ul className="list-unstyled links mb-4">
-<li><a href="tel://0728131955">+25428131955</a></li>
-
-<li><a href="mailto://dianaiminza.99@gmail.com">[dianaiminza.99@gmail.com]</a></li>
-</ul>
-<h3>Connect</h3>
-<ul className="list-unstyled social">
-<li><a href="https://instagram.com/vanilla-restaurant"rel="noopener noreferrer"target="_blank">
-    <i className="fa fa-instagram-square"aria-hidden="true"/>
-</a></li>
-<li><a href="tel://0728131955" rel="noopener noreferrer"target="_blank">
-    <i className="fa fa-whatsapp-square"aria-hidden="true"/>
-</a></li>
-
-<li><a href="https://www.facebook.com/vanilla-restaurant/"rel="noopener noreferrer"target="_blank">
-    <i className="fa fa-facebook-square"aria-hidden="true"/>
-</a></li>
-</ul>
-</div> 
-</div> 
+                <footer className="footer">All right reserved.</footer>
+                
             </div>
         )
     }
