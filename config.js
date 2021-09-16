3bc532bf116f8c7c25d7212d23353a8eeca95a1e
Dianaiminza <dianaiminza.99@gmail.com>
2021-09-16 11:44:08 +0300

    my first commit

--- config.js
+++ config.js
@@ -7,7 +7,9 @@ module.exports={
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbUser:Captain@cluster0.w11hf.mongodb.net/food?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'captain',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'captain',
+  GOOGLE_API_KEY:process.env.GOOGLE_API_KEY || '',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
   secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
+
 };
 
