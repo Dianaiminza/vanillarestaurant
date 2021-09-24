2ae467b6e135855eea6df3228f7f2e43021e9be5
Dianaiminza <dianaiminza.99@gmail.com>
2021-09-24 21:46:23 +0300

    my first commit

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -40,23 +40,23 @@ import {
       default: return state;
     }
   }
-  function myOrderListReducer(state = { orders: [] }, action) {
+  function myOrderListReducer(state = {}, action) {
     switch (action.type) {
       case MY_ORDER_LIST_REQUEST:
         return { loading: true };
       case MY_ORDER_LIST_SUCCESS:
-        return { loading: false, orders: action.payload, success: true };
+        return { loading: false, order: action.payload, success: true };
       case MY_ORDER_LIST_FAIL:
         return { loading: false, error: action.payload };
       default: return state;
     }
   }
-  function orderListReducer(state = { orders: [] }, action) {
+  function orderListReducer(state = {}, action) {
     switch (action.type) {
       case ORDER_LIST_REQUEST:
         return { loading: true };
       case ORDER_LIST_SUCCESS:
-        return { loading: false, orders: action.payload, success: true };
+        return { loading: false, order: action.payload, success: true };
       case ORDER_LIST_FAIL:
         return { loading: false, error: action.payload };
       default: return state;
