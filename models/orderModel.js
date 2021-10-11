56e96dbe109aea69ccc33287fed338781f30ba5a
Dianaiminza <dianaiminza.99@gmail.com>
2021-10-12 00:38:56 +0300

    my first commit

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,49 +1,55 @@
-var  mongoose =require('mongoose');
-
-const orderSchema = new mongoose.Schema(
-  {
-    orderItems: [
-      {
-        name: { type: String},
-        qty: { type: Number},
-        image: { type: String },
-        price: { type: Number },
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Product',
-          required: true,
-        },
-      },
-    ],
-    shipping: {
-      address: { type: String},
+var mongoose =require('mongoose');
+const shippingSchema = {
+  address: { type: String},
   estate: { type: String },
   postalCode: { type: String },
   phonenumber: { type: String },
   
-    },
-    paymentMethod: { type: String },
-    paymentResult: {
-      id: String,
-      status: String,
-      update_time: String,
-      email_address: String,
-    },
-    itemsPrice: { type: Number },
-    shippingPrice: { type: Number },
-    taxPrice: { type: Number},
-    totalPrice: { type: Number },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    
-    isPaid: { type: Boolean, default: false },
-    paidAt: { type: Date },
-    isDelivered: { type: Boolean, default: false },
-    deliveredAt: { type: Date },
-    
-  },
-  {
-    timestamps: true,
+};
+
+const paymentSchema = {
+  paymentMethod: { type: String },
+  paymentResult: {
+    id: String,
+    status: String,
+    update_time: String,
+    email_address: String,
   }
-);
-const orderModel = mongoose.model('Order', orderSchema);
+};
+
+const orderItemSchema = new mongoose.Schema({
+  name: { type: String},
+  qty: { type: Number},
+  image: { type: String },
+  price: { type: String},
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+});
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  orderItems: [orderItemSchema],
+  shipping: shippingSchema,
+  payment: paymentSchema,
+  itemsPrice: { type: Number },
+  taxPrice: { type: Number },
+  shippingPrice: { type: Number },
+  totalPrice: { type: Number },
+  isPaid: { type: Boolean, default: false },
+  paidAt: { type: Date },
+  isDelivered: { type: Boolean, default: false },
+  deliveredAt: { type: Date },
+}, {
+  timestamps: true
+});
+
+const orderModel = mongoose.model("Order", orderSchema);
 module.exports=orderModel;
+
+
+
+
+
